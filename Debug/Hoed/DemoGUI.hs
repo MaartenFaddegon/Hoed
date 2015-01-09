4ecad8c41a0af46b04578fb56cdf3f983ca6e51b
Maarten Faddegon <mf357@kent.ac.uk>
2015-01-09 09:44:23 +0000

    rewrite GUI: only show details of one statement and allow others to be accessed through dropdown menu

--- Debug/Hoed/DemoGUI.hs
+++ Debug/Hoed/DemoGUI.hs
@@ -16,30 +16,77 @@ import System.Process(system)
 import Data.IORef
 import Data.List(intersperse)
 
+
+--------------------------------------------------------------------------------
+
 preorder :: CompGraph -> [Vertex]
 preorder = getPreorder . getDfs
+
+showStmtN :: [Vertex] -> UI.Element -> Int -> UI ()
+showStmtN vs e i = do UI.element e # UI.set UI.text s; return ()
+  where s = if i < length vs then showCompStmts (vs !! i)
+                             else "Internal error: " ++ show i ++ " is out of range."
 
 demoGUI :: [(String,String)] -> IORef CompGraph -> Window -> UI ()
 demoGUI sliceDict treeRef window
   = do return window # UI.set UI.title "Hoed debugging session"
        UI.addStyleSheet window "debug.css"
-       img <- UI.img 
-       redraw img treeRef
-       img' <- UI.center #+ [UI.element img]
-       buttons <- UI.div #. "buttons"
-       nowrap  <- UI.div #. "nowrap"  #+ (map UI.element [buttons,img'])
-       UI.getBody window #+ (map UI.element [nowrap])
 
+       -- Get a list of vertices from the computation graph
        tree <- UI.liftIO $ readIORef treeRef
        let ns = filter (not . isRoot) (preorder tree)
-       ts <- toElems sliceDict ns
-       ds <- mapM (uncurry divpack) (zip ts (cycle [Odd,Even]))
-       UI.element buttons # UI.set UI.children ds
-       mapM_ (onClick buttons img treeRef Right) 
-             (zip (corButtons ts) (reverse ns))
-       mapM_ (onClick buttons img treeRef Wrong)
-             (zip (wrnButtons ts) (reverse ns))
 
+        -- Keep track of the current vertex
+       currentVertex <- UI.liftIO $ newIORef (0 :: Int)
+       let getCurrentVertex = UI.liftIO $ do n <- readIORef currentVertex; return (ns !! n)
+           setCurrentVertex n = UI.liftIO $ writeIORef currentVertex n
+
+       -- Draw the computation graph
+       img  <- UI.img 
+       redraw img treeRef
+       img' <- UI.center #+ [UI.element img]
+
+       -- Show computation statement(s) of current vertex
+       compStmt <- UI.pre
+       let showStmt = showStmtN ns compStmt
+       showStmt 0
+
+       -- Menu to select which statement to show
+       menu <- UI.select
+       populateMenu menu treeRef
+       listenToSelectionChange menu showStmt setCurrentVertex
+
+       -- Buttons to judge the current statement
+       right <- UI.button # UI.set UI.text "right"
+       wrong <- UI.button # UI.set UI.text "wrong"
+       onClick right menu img treeRef Right getCurrentVertex
+       onClick wrong menu img treeRef Wrong getCurrentVertex
+
+       -- Populate the main screen
+       hr <- UI.hr
+       UI.getBody window    #+ (map UI.element [menu, right, wrong, compStmt, hr,img'])
+       return ()
+
+       -- buttons <- UI.div    #. "buttons"
+       -- nowrap  <- UI.div    #. "nowrap"  #+ (map UI.element [buttons,img'])
+
+       -- ts <- toElems sliceDict ns
+       -- ds <- mapM (uncurry divpack) (zip ts (cycle [Odd,Even]))
+       -- UI.element buttons # UI.set UI.children ds
+       -- mapM_ (onClick buttons img treeRef Right) 
+       --       (zip (corButtons ts) (reverse ns))
+       -- mapM_ (onClick buttons img treeRef Wrong)
+       --       (zip (wrnButtons ts) (reverse ns))
+populateMenu :: UI.Element -> IORef CompGraph -> UI ()
+populateMenu menu treeRef = do
+       g <- UI.liftIO $ readIORef treeRef
+       let ns = filter (not . isRoot) (preorder g)
+       let fs = faultyVertices g
+       ops  <- mapM (\s->UI.option # UI.set UI.text s) $ map (summarizeVertex fs) ns
+       -- UI.element menu # UI.set UI.children [ops] -- (map UI.element ops)
+       (UI.element menu) # UI.set UI.children []
+       UI.element menu #+ (map UI.element ops)
+       return ()
 
 --              Slice      Hr         CompStmt   Right    Wrong
 type ElemSet = (UI.Element,UI.Element,UI.Element,UI.Element,UI.Element)
@@ -52,14 +99,32 @@ divpack (e1,e2,e3,e4,e5) x
     where lbl Odd  = "odd"
           lbl Even = "even"
 
-onClick :: UI.Element -> UI.Element -> IORef CompGraph -> Judgement
-        -> (UI.Element,Vertex) -> UI ()
-onClick buttons img treeRef status (b,n) 
-  = do on UI.click b $ \_ -> do 
-        updateTree img treeRef (\tree -> markNode tree n status)
-        -- UI.element b # UI.set UI.text "I have been clicked!"
-        -- UI.element buttons # UI.set UI.children []
-        
+onClick :: UI.Element -> UI.Element -> UI.Element -> IORef CompGraph -> Judgement -> IO Vertex 
+           -> UI ()
+onClick b menu img treeRef j getCurrentVertex = do
+  on UI.click b $ \_ -> do
+        n <- UI.liftIO getCurrentVertex
+        updateTree img treeRef (\tree -> markNode tree n j)
+        populateMenu menu treeRef
+
+-- onClick :: UI.Element -> IORef CompGraph -> Judgement -> (UI.Element,Vertex) -> UI ()
+-- onClick img treeRef status (b,n) 
+--   = do on UI.click b $ \_ -> do 
+--         updateTree img treeRef (\tree -> markNode tree n status)
+--         -- UI.element b # UI.set UI.text "I have been clicked!"
+--         -- UI.element buttons # UI.set UI.children []
+
+listenToSelectionChange :: UI.Element -> (Int -> UI ()) -> (Int -> IO ()) -> UI ()
+listenToSelectionChange menu showStmt setN = do
+  on UI.selectionChange menu $ \mi -> case mi of
+        Just i  -> do UI.liftIO $ setN i
+                      showStmt i
+        Nothing -> return ()
+
+    -- op <- UI.option # UI.set UI.text ("You selected option " ++ show mi)
+    -- UI.element menu #+ [UI.element op]
+    -- UI.element menutxt # UI.set UI.text ("You selected option " ++ show mi)
+    -- return ()
 
 -- MF TODO: We may need to reconsider how Vertex is defined,
 -- and how we determine equality. I think it could happen that
@@ -79,6 +144,11 @@ corButtons = foldl (\es (_,_,_,e,_) -> e : es) []
 wrnButtons :: [ElemSet] -> [UI.Element]
 wrnButtons = foldl (\es (_,_,_,_,e) -> e : es) []
 
+-- toOptions :: [String] -> UI [UI.Element]
+-- toOptions = mapM toOption'
+--   where toOption' :: String -> UI UI.Element
+--         toOption' s = UI.option # UI.set UI.text s
+
 toElems :: [(String,String)] -> [Vertex] -> UI [ElemSet]
 toElems sliceDict xs = mapM (toElem sliceDict) xs
 
@@ -93,6 +163,29 @@ toElem sliceDict v
    where getSlice e = case equLabel e `lookup` sliceDict of
               Nothing -> ""
               Just s  -> s
+
+data MaxStringLength = ShorterThan Int | Unlimited
+
+shorten :: MaxStringLength -> String -> String
+shorten Unlimited s = s
+shorten (ShorterThan l) s
+          | length s < l = s
+          | l > 3        = take (l - 3) s ++ "..."
+          | otherwise    = take l s
+
+-- MF TODO: Maybe we should do something smart with witespace substitution here?
+noNewlines :: String -> String
+noNewlines = filter (/= '\n')
+
+showCompStmts :: Vertex -> String
+showCompStmts = commas . map show . equations
+
+summarizeVertex :: [Vertex] -> Vertex -> String
+summarizeVertex fs v = shorten (ShorterThan 27) (noNewlines $ showCompStmts v) ++ s
+  where s = if v `elem` fs then " !!" else case status v of
+              Unassessed     -> " ??"
+              Wrong          -> " :("
+              Right          -> " :)"
 
 updateTree :: UI.Element -> IORef CompGraph -> (CompGraph -> CompGraph)
            -> UI ()
@@ -111,12 +204,11 @@ redraw img treeRef
        UI.element img # UI.set UI.src url
        return ()
 
-  where shw g = showWith g (showVertexSimple $ faultyVertices g) showArc
+  where shw g = showWith g (summarizeVertex $ faultyVertices g) showArc
         showVertex :: [Vertex] -> Vertex -> String
         showVertex _ Root = "root"
         showVertex fs v = showStatus fs v ++ ":\n" ++ showCompStmts v
                           ++ "\nwith stack " ++ (show . equStack . head . equations $ v)
-        showCompStmts = commas . equations
 
         showVertexSimple fs v = showStatus fs v ++ ":" ++ showCompStmtsSimple v
         showCompStmtsSimple = commas . (map equLabel) . equations
@@ -125,14 +217,14 @@ redraw img treeRef
           | v `elem` fs = "Faulty"
           | otherwise   = (show . status) v
 
-        commas [e] = show e
-        commas es  = foldl (\acc e-> acc ++ show e ++ ", ") "{" (init es) 
-                             ++ show (last es) ++ "}"
-
-
         -- showVertex = show
         -- showVertex = (foldl (++) "") . (map show) . equations
         showArc _  = ""
+
+commas :: [String] -> String
+commas [e] = e
+commas es  = foldl (\acc e-> acc ++ e ++ ", ") "{" (init es) 
+                     ++ show (last es) ++ "}"
 
 faultyVertices :: CompGraph -> [Vertex]
 faultyVertices = findFaulty_dag getStatus
